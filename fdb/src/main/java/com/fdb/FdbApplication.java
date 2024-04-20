3e2e6c6d19f2b73756585adcd9b996d1504ee8e2
sumanth <iamsumanth01@gmail.com>
2024-04-20 15:43:31 +0530

    setting up the backend maven from spring initilzr

---
+++ fdb/src/main/java/com/fdb/FdbApplication.java
@@ -0,0 +1,13 @@
+package com.fdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FdbApplication.class, args);
+	}
+
+}
